e8a8f386c3501f958d7793cbba0aac36b3c6b3ac
Florian Castellane <583664+LIII-XXII@users.noreply.github.com>
2024-04-09 16:06:41 +0900

    Create prusa-connect.sh

---
+++ prusa-connect.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+set -Eeu
+set -o pipefail
+shopt -s extdebug
+
+FINGERPRINT="your-printer-fingerprint"
+TOKEN="your-prusa-connect-webcam-token"
+# PRINTER_UUID="abcdef01-2345-6789-abcd-ef0123456"
+
+# shellcheck disable=SC2154
+trap 'declare rc=$?;
+      >&2 echo "Unexpected error (exit-code $rc) executing $BASH_COMMAND at ${BASH_SOURCE[0]} line $LINENO";
+      exit $rc' ERR
+
+main () {
+
+  while true
+  do
+    if rpicam-still \
+      --width 2304 --height 1296 \
+      -v 0 \
+      -t 1000 \
+      --nopreview \
+      -o - \
+    | curl  https://webcam.connect.prusa3d.com/c/snapshot  \
+      -X PUT \
+      -L --no-progress-meter --fail-with-body \
+      -H "Content-Type: image/jpg" \
+      -H "Fingerprint: $FINGERPRINT" \
+      -H "Token: $TOKEN" \
+      --data-binary @-
+    then
+      echo
+      printf "."
+    else
+      echo
+      echo "oops, failed"
+      sleep 50
+    fi
+    sleep 10
+  done
+}
+
+main "$@"
